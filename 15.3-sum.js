2b9f58b49d2ae2e3f4e2e5f744457081205174b0
jimmylu1 <jimmy88lu@gmail.com>
2020-02-05 19:50:03 -0800

    3-sum

--- 15.3-sum.js
+++ 15.3-sum.js
@@ -10,19 +10,25 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
+  //sorted array so we can implement binary search
     nums.sort((a, b) => a - b);
     let res = [];
 
+    //loop through our sorted array and start pointer at next idx and end pointer 
+    //end of array
     for(let i = 0; i < nums.length; i++) {
       let start = i + 1, end = nums.length - 1;
+      //binary search
       while(start < end) {
         let currSum = nums[i] + nums[start] + nums[end];
         if(currSum === 0) {
           res.push([nums[i], nums[start], nums[end]]);
+          //need to check for duplicates, if nums[start] == nums[start + 1] need to increment start
           while(start < end && nums[start] === nums[start + 1]) {
             start++;
           }
           start++;
+          //same check as start for ending
           while(start < end && nums[end] === nums[end - 1]) {
             end--;
           }
@@ -33,6 +39,7 @@ var threeSum = function(nums) {
           end--;
         }
       }
+      //need to check if current number is the same number as previously checked
       while(i < nums.length && nums[i] === nums[i + 1]) {
         i++;
       }
