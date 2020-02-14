4e9729ba0c93e6fe6435a236adf406ed4a2bb8ba
jimmylu1 <jimmy88lu@gmail.com>
2020-02-13 17:16:19 -0800

    647.palindromic-substrings.js

--- 647.palindromic-substrings.js
+++ 647.palindromic-substrings.js
@@ -7,22 +7,33 @@
  * @param {string} s
  * @return {number}
  */
-var countSubstrings = function(s) {
-  let count = s.length;
-  let next;
+var countSubstrings = function (s) {
+  // in case s is null or empty, return 0
+  if (!s) return 0;
 
-  var ispal = function(left, right) {
-    while (left >= 0 && right < s.length && s[right] === s[left]) {
-      count++;
+  let count = 0;
+
+  for (let i = 0; i < s.length; ++i) {
+    // count all possible odd length palindromic substrings whose mid-point is at i
+    count += countPalindrome(s, i, i);
+    // count all possible even length palindromic substrings whose mid-point is in between i and i + 1
+    count += countPalindrome(s, i, i + 1);
+  }
+  return count;
+};
+
+var countPalindrome = function (s, left, right) {
+  let count = 0;
+  // expand substring to both side, each time increase size by 2
+  // if substring after expansion is still palindromic, keep expanding and update count
+  // otherwise, stop expansion and return the count
+  while (left >= 0 && right < s.length) {
+    if (s[left] === s[right]) {
       left--;
       right++;
+      count += 1;
     }
-  };
-
-  for (let i = 0; i < s.length; i++) {
-    next = i + 1;
-    ispal(i - 1, next);
-    ispal(i, next);
+    else break;
   }
   return count;
 }
