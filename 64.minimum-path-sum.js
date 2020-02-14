ef049b286312f9a805188f770abf8dbf81ca25d9
jimmylu1 <jimmy88lu@gmail.com>
2020-02-14 01:12:59 -0800

    64.minimum-path-sum.js

--- 64.minimum-path-sum.js
+++ 64.minimum-path-sum.js
@@ -8,46 +8,23 @@
  * @return {number}
  */
 var minPathSum = function(grid) {
-  if(grid.length === 0) return 0;
-
-  let dp = new Array(grid.length).fill(0).map(() => new Array(grid[0].length).fill(0))
-
+  let dp = [...grid];
   for(let i = 0; i < grid.length; i++) {
     for(let j = 0; j < grid[i].length; j++) {
-      dp[i][j] += grid[i][j]
-
       if(i > 0 && j > 0) {
-        dp[i][j] += Math.min(dp[i - 1][j], dp[i][j - 1])
+        dp[i][j] += Math.min(dp[i - 1][j], dp[i][j - 1]);
       } else if(i > 0) {
-        dp[i][j] += dp[i - 1][j]
+        dp[i][j] += dp[i - 1][j];
       } else if(j > 0) {
-        dp[i][j] += dp[i][j - 1]
+        dp[i][j] += dp[i][j - 1];
       }
     }
   }
-return dp[grid.length - 1][grid[0].length - 1]
+  return dp[dp.length - 1][dp[0].length - 1];
 };
 
-// var minPathSum = function(grid) {
-//   if(grid === null || grid.length === 0) return 0;
-
-//   //new dp array
-//   let dp = new Array(grid.length).fill(0).map(() => new Array(grid[0].length).fill(0))
-
-//   //traverse through dp array to populate
-//   for(let i = 0; i < dp.length; i++) {
-//     for(let j = 0; j < dp[i].length; j++) {
-//       dp[i][j] += grid[i][j]
-//       //not in 0th row
-//       //somewhere in grid not in 0th col/row
-//       if(i > 0 && j > 0) {
-//         dp[i][j] += Math.min(dp[i - 1][j], dp[i][j - 1])
-//       } else if(i > 0) {
-//         dp[i][j] += dp[i - 1][j]
-//       } else if(j > 0) {
-//         dp[i][j] += dp[i][j - 1]
-//       }
-//     }
-//   }
-//   return dp[grid.length - 1][grid[0].length - 1]
-// }
+// console.log(minPathSum([
+//   [1, 3, 1],
+//   [1, 5, 1],
+//   [4, 2, 1]
+// ]))
