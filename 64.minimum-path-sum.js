2c694beacbf717624982a14e4745a7a66fb5f1cd
jimmylu1 <jimmy88lu@gmail.com>
2020-02-17 21:05:52 -0800

    64.minimum-path-sum.js

--- 64.minimum-path-sum.js
+++ 64.minimum-path-sum.js
@@ -9,10 +9,13 @@
  */
 var minPathSum = function(grid) {
   let dp = [...grid];
+
+  if(grid.length === 0) return 0;
+
   for(let i = 0; i < grid.length; i++) {
     for(let j = 0; j < grid[i].length; j++) {
-      if(i > 0 && j > 0) {
-        dp[i][j] += Math.min(dp[i - 1][j], dp[i][j - 1]);
+      if (i > 0 && j > 0) {
+        dp[i][j] += Math.min(grid[i - 1][j], grid[i][j - 1]);
       } else if(i > 0) {
         dp[i][j] += dp[i - 1][j];
       } else if(j > 0) {
@@ -22,6 +25,9 @@ var minPathSum = function(grid) {
   }
   return dp[dp.length - 1][dp[0].length - 1];
 };
+
+
+
 
 // console.log(minPathSum([
 //   [1, 3, 1],
